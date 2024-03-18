d72daae7e480e4f8433e9b9ee03443a10d68a64c
Jay.D <djccnt15@gmail.com>
2024-03-19 00:27:41 +0900

    create custom annotation

---
+++ src/main/java/com/example/study_webflux/annotation/Converter.java
@@ -0,0 +1,20 @@
+package com.example.study_webflux.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Service;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+// this annotation is for annotating type of logic
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Service  // register this class and implements as a spring bean
+public @interface Converter {
+
+    @AliasFor(annotation = Service.class)
+    String value() default "";
+}
