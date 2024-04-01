e4938b0402405644785eb174da22c8631d8fb147
Jay.D <djccnt15@gmail.com>
2024-04-02 00:20:27 +0900

    create temporal redirect to swagger from root index

---
+++ webflux/src/main/java/com/example/study_webflux/domain/index/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.example.study_webflux.domain.index.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(path = "/")
+public class IndexController {
+    
+    @GetMapping
+    public ResponseEntity<?> redirect() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("webjars/swagger-ui/index.html"));
+        return new ResponseEntity<>(headers, HttpStatus.TEMPORARY_REDIRECT);
+    }
+}
