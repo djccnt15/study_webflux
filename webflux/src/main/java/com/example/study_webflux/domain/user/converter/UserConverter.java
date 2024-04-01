f37c622e4f29807589d9dc0cd0b9e95801dff6c0
Jay.D <djccnt15@gmail.com>
2024-04-02 00:36:18 +0900

    update UserConverter create toEntity func

--- webflux/src/main/java/com/example/study_webflux/domain/user/converter/UserConverter.java
+++ webflux/src/main/java/com/example/study_webflux/domain/user/converter/UserConverter.java
@@ -1,6 +1,7 @@
 package com.example.study_webflux.domain.user.converter;
 
 import com.example.study_webflux.annotation.Converter;
+import com.example.study_webflux.domain.user.model.UserCreateRequest;
 import com.example.study_webflux.domain.user.model.UserResponse;
 import com.example.study_webflux.repository.user.UserEntity;
 import lombok.RequiredArgsConstructor;
@@ -9,13 +10,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserConverter {
     
+    public UserEntity toEntity(UserCreateRequest request) {
+        var userEntity = UserEntity.builder()
+            .userName(request.getUserName())
+            .email(request.getEmail())
+            .build();
+        
+        return userEntity;
+    }
+    
     public UserResponse toResponse(UserEntity userEntity) {
-        return UserResponse.builder()
+        var user = UserResponse.builder()
             .id(userEntity.getId())
             .userName(userEntity.getUserName())
             .email(userEntity.getEmail())
             .createdAt(userEntity.getCreatedAt())
             .updatedAt(userEntity.getUpdatedAt())
             .build();
+        
+        return user;
     }
 }
